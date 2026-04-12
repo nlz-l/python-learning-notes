70a825efa30ad8b8f3582f3bb0e0434fde33cc78
nlz <473213284@qq.com>
2026-04-12 17:23:18 +0800

    更新

--- 5.蓝桥/自练/test/l1.py
+++ 5.蓝桥/自练/test/l1.py
@@ -1,7 +1,11 @@
-sum1 = 0
-n = 0
-a = 0
-for i in range(496):
-    n += i
-    a += n
-    print(a)
+import os
+import sys
+
+count = 0
+a = ''
+for i in range(1, 2027):
+    a+=str(i)
+    b = int(a)
+    if b % 26 == 0:
+        count += 1
+print(count)
